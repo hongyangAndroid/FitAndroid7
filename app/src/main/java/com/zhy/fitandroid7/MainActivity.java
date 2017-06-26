6ff70f7a8abe28e2f72f5ff22b85474cb3686a31
zhanghongyang01 <zhanghongyang01@baidu.com>
2017-06-26 11:08:01 +0800

    增加权限申请的代码

--- app/src/main/java/com/zhy/fitandroid7/MainActivity.java
+++ app/src/main/java/com/zhy/fitandroid7/MainActivity.java
@@ -1,15 +1,21 @@
 package com.zhy.fitandroid7;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.zhy.base.fileprovider.FileProvider7;
 
@@ -20,8 +26,13 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_TAKE_PHOTO = 0x110;
+    private static final int REQ_PERMISSION_CODE_SDCARD = 0X111;
+    private static final int REQ_PERMISSION_CODE_TAKE_PHOTO = 0X112;
 
+    private String mCurrentPhotoPath;
     private ImageView mIvPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +41,54 @@ public class MainActivity extends AppCompatActivity {
 
         mIvPhoto = (ImageView) findViewById(R.id.id_iv);
 
-        // 6.0原生系统自行打开SDCard权限!
     }
 
     public void installApk(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQ_PERMISSION_CODE_SDCARD);
+
+        } else {
+            installApk();
+        }
+
+    }
+
+
+    private void installApk() {
         // 需要自己修改安装包路径
         File file = new File(Environment.getExternalStorageDirectory(),
-                "testandroid7-debug.apk");
+                "app-debug.apk");
         Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         FileProvider7.setIntentDataAndType(this,
                 intent, "application/vnd.android.package-archive", file, true);
+
         startActivity(intent);
     }
 
 
-    private static final int REQUEST_CODE_TAKE_PHOTO = 0x110;
-    private String mCurrentPhotoPath;
-
     public void takePhotoNoCompress(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQ_PERMISSION_CODE_TAKE_PHOTO);
+
+        } else {
+            takePhotoNoCompress();
+        }
+    }
+
+
+    private void takePhotoNoCompress() {
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             String filename = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA)
@@ -60,6 +101,29 @@ public class MainActivity extends AppCompatActivity {
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
             startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQ_PERMISSION_CODE_SDCARD) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                installApk();
+            } else {
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        } else if (requestCode == REQ_PERMISSION_CODE_TAKE_PHOTO) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePhotoNoCompress();
+            } else {
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
     @Override
